f8cd20d51de54b2fe9c6c19627cb8c60053deac1
mushegharustamyan <mushegh.arustamyan@gmail.com>
2022-08-15 13:17:11 +0400

    next section adding

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,7 +1,14 @@
-let burger = document.querySelector(".burger_menu") ;
-burger.addEventListener("click" , (e) => {
-    let main = document.querySelector("section") ;
-    main.classList.toggle("blur") ;
-    let nav = document.querySelector(".navigation_resp") ;
-    nav.classList.toggle("active") ;
-})
+function activateBurgerMenu () {
+    let burger = document.querySelector(".burger_menu") ;
+    burger.addEventListener("click" , (e) => {
+        let main = document.querySelectorAll(".section") ;
+        main.forEach(elem => {
+            elem.classList.toggle("blur") ;
+        })
+        let nav = document.querySelector(".navigation_resp") ;
+        nav.classList.toggle("active") ;
+    })
+}
+
+activateBurgerMenu() ;
+
